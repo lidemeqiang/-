ec058c08ee843a0f798091c96f9548af1b9e4dc4
lanou3g <zhaoce@mengqingfei.local>
2016-06-13 10:10:33 +0800

    测试新建了一个 Model 类 详情详情页面

---
+++ 候鸟/候鸟/DetailsModel.h
@@ -0,0 +1,25 @@
+//
+//  DetailsModel.h
+//  候鸟
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 david. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetailsModel : NSObject
+
+
+@property(nonatomic,strong)NSData *imageView;
+
+
+
+
+
+
+
+
+
+
+@end
---
+++ 候鸟/候鸟/DetailsModel.m
@@ -0,0 +1,13 @@
+//
+//  DetailsModel.m
+//  候鸟
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 david. All rights reserved.
+//
+
+#import "DetailsModel.h"
+
+@implementation DetailsModel
+
+@end
